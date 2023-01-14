abf2ff414749187f732f1557a6f5a76893c0ee9e
Sebastian Nunez <02snunez@gmail.com>
2023-01-14 18:12:23 -0500

    basic books model

---
+++ backend/models/booksModel.js
@@ -0,0 +1,13 @@
+const mongoose = require("mongoose");
+
+const booksSchema = mongoose.Schema({
+  title: String,
+  author: String,
+  genre: [String],
+  copiesSold: Number,
+  rating: Number,
+  publisher: String,
+  price: Number,
+});
+
+module.exports = mongoose.model("Books", booksSchema);
