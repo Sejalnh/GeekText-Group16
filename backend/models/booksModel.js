e9f47069736234e02e5e95c5314d0ace62b1f48a
Sebastian Nunez <02snunez@gmail.com>
2023-01-15 12:01:13 -0500

    specified required attributes for book model

--- backend/models/booksModel.js
+++ backend/models/booksModel.js
@@ -1,13 +1,34 @@
 const mongoose = require("mongoose");
 
 const booksSchema = mongoose.Schema({
-  title: String,
-  author: String,
-  genre: String,
-  copiesSold: Number,
-  rating: Number,
-  publisher: String,
-  price: Number,
+  title: {
+    type: String,
+    required: true,
+  },
+  author: {
+    type: String,
+    required: true,
+  },
+  genre: {
+    type: String,
+    required: true,
+  },
+  copiesSold: {
+    type: Number,
+    required: true,
+  },
+  rating: {
+    type: Number,
+    required: true,
+  },
+  publisher: {
+    type: String,
+    required: true,
+  },
+  price: {
+    type: Number,
+    required: true,
+  },
 });
 
 module.exports = mongoose.model("Books", booksSchema);
