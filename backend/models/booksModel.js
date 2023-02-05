5d2fe979713ce8ae1bdcec51065a9a994e67f46b
Sebastian Nunez <02snunez@gmail.com>
2023-02-05 06:18:38 -0500

    isbn numbers should be integers and added min/max length for a description

--- backend/models/booksModel.js
+++ backend/models/booksModel.js
@@ -75,12 +75,15 @@ const booksSchema = mongoose.Schema({
   isbn: {
     type: Number,
     required: true,
-    get: v => Math.round(v),
-    set: v => Math.round(v)
+    get: v => Math.floor(v),
+    set: v => Math.floor(v)
   },
   description: {
     type: String,
-    required: true,
+    default: "No description found!",
+    required: false,
+    minLength: 0,
+    maxLength: 1000,
     trim: true
   },
   yearPublished: {
