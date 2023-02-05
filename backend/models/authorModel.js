363fe2e3a5c0911e36f7d1fb0ad240d45f0b57d6
Sebastian Nunez <02snunez@gmail.com>
2023-02-05 08:09:34 -0500

    author model created

---
+++ backend/models/authorModel.js
@@ -0,0 +1,27 @@
+const mongoose = require("mongoose");
+
+const authorModel = mongoose.Schema({
+  firstname: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  lastname: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  biography: {
+    type: String,
+    default: "No biography provided!",
+    required: false,
+    trim: true
+  },
+  publisher: {
+    type: String,
+    required: true,
+    trim: true
+  }
+});
+
+module.exports = mongoose.model("Author", authorModel);
