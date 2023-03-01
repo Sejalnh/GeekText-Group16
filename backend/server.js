13de2e4a9cefc7b9742b8b17fd8271bf64725861
Raymond Napoles <rnapo004@fiu.edu>
2023-03-01 15:12:30 -0500

    added incomplete version of add book to shopping cart sub feature

--- backend/server.js
+++ backend/server.js
@@ -155,9 +155,22 @@ app.put("/books/discount", async (req, res) => {
 // 3.1 Retrieve the subtotal price of all items in the user’s shopping cart. 
 
 // 3.2 Add a book to the shopping cart. 
+app.post("/users/username/:username/shoppingCart/add/:ISBN",async(req, res)=>{
+  const username= req.params.username;
+  const newBook= req.params.ISBN;
 
+  try{
+   const shoppingCart= await User.findOneAndUpdate({username},{$push:{"shoppingCart":newBook}})
+    res.status(200).json(shoppingCart);
+  }catch(error) {
+    res.status(404).json({message: 'Did not work'});
+  }
+
+
+})
 // 3.3 Retrieve the list of book(s) in the user’s shopping cart. 
 
 // 3.4 Delete a book from the shopping cart instance for that user.
+
 // ----------------------------------------------------------------------------------
 app.listen(PORT, () => console.log(`Server started on port ${PORT}...`));
