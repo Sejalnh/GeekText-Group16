0021513322475916d0f2c608bafa5cf35cfee24b
sebastian-nunez <02snunez@gmail.com>
2023-03-12 16:19:39 -0400

    feature 1.4 implemented. discounts

--- backend/controllers/BookBrowsingController.js
+++ backend/controllers/BookBrowsingController.js
@@ -37,11 +37,28 @@ class BookBrowsingController {
       res.status(404).json({ message: error });
     }
   };
+
+  // 1.4 Discount books by publisher.
+  static applyDiscount = async (req, res) => {
+    const percentDiscount = 1.0 - parseFloat(req.body.percentDiscount) / 100.0;
+    const publisher_param = req.body.publisher;
+
+    try {
+      const books = await Books.updateMany(
+        { publisher: publisher_param },
+        { $mul: { price: percentDiscount } }
+      );
+      res.status(200).json(books);
+    } catch (error) {
+      res.status(404).json({ message: error });
+    }
+  };
 }
 
 // routes
 router.get("/books/genre/:genre", BookBrowsingController.getByGenre);
 router.get("/books/top10", BookBrowsingController.getTopTen);
 router.get("/books/rating/:rating", BookBrowsingController.getByRating);
+router.put("/books/discount", BookBrowsingController.applyDiscount);
 
 module.exports = router;
