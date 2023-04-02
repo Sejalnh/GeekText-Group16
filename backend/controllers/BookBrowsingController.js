89e955a2d69179991dd32484a8e71438b80355be
sebastian-nunez <02snunez@gmail.com>
2023-04-02 12:54:52 -0400

    book browsing fixed

--- backend/controllers/BookBrowsingController.js
+++ backend/controllers/BookBrowsingController.js
@@ -1,87 +1,53 @@
 const express = require("express");
 const router = express.Router();
 const Books = require("../models/booksModel");
-const Author = require("../models/authorModel");
 
-// Users can see informative and enticing details about a book
-class BookDetailsController {
-  // 4.1 An administrator must be able to create a book with the book ISBN, book name, book description, price, author, genre, publisher , year published and copies sold.
-  static createBook = async (req, res) => {
-    const {
-      title,
-      ISBN,
-      author,
-      genre,
-      copiesSold,
-      rating,
-      publisher,
-      price,
-      description,
-      yearPublished,
-      ratings,
-      comments
-    } = req.body;
-
-    const books = new Books({
-      title,
-      ISBN,
-      author,
-      genre,
-      copiesSold,
-      rating,
-      publisher,
-      price,
-      description,
-      yearPublished,
-      ratings,
-      comments
-    });
+// Users will have a simple and enjoyable way to discover new books and Authors and sort results.
+class BookBrowsingController {
+  // 1.1 Retrieve List of Books by Genre
+  static getByGenre = async (req, res) => {
+    const genre = req.params.genre;
 
     try {
-      await books.save();
-      res.status(200).send("Book added!");
-    } catch (error) {
-      res.status(404).send(error.message);
-    }
-  };
-
-  // 4.2 Must be able retrieve a book’s details by the ISBN
-  static getByISBN = async (req, res) => {
-    const ISBN = req.params.ISBN; // retrieve the genre from the URL
-
-    try {
-      const books = await Books.find({ ISBN }); // wait for books to be retrieved ASYNCHRONOUSLY
+      const books = await Books.find({ genre });
       res.status(200).json(books);
     } catch (error) {
       res.status(404).json({ message: error });
     }
   };
 
-  // 4.3 An administrator must be able to create an author with first name, last name, biography and publisher
-  static createAuthor = async (req, res) => {
-    const { firstname, lastname, biography, publisher } = req.body;
-
-    const author = new Author({
-      firstname,
-      lastname,
-      biography,
-      publisher
-    });
-
+  // 1.2 Retrieve List of Top Sellers (Top 10 books that have sold the most copied)
+  static getTopTen = async (req, res) => {
     try {
-      await author.save();
-      res.status(200).send("Author added!");
+      const books = await Books.find().sort({ copiesSold: -1 }).limit(10);
+      res.status(200).json(books);
     } catch (error) {
-      res.status(404).send(error.message);
+      res.status(404).json({ message: error });
     }
   };
 
-  // 4.4 Must be able to retrieve a list of books associated with an author
-  static getByAuthor = async (req, res) => {
-    const author = req.params.author; // retrieve the genre from the URL
+  // 1.3 Retrieve List of Books for a particular rating and higher
+  static getByRating = async (req, res) => {
+    const rating_param = parseFloat(req.params.rating);
 
     try {
-      const books = await Books.find({ author }); // wait for books to be retrieved ASYNCHRONOUSLY
+      const books = await Books.find({ rating: { $gte: rating_param } });
+      res.status(200).json(books);
+    } catch (error) {
+      res.status(404).json({ message: error });
+    }
+  };
+
+  // 1.4 Discount books by publisher.
+  static applyDiscount = async (req, res) => {
+    const percentDiscount = 1.0 - parseFloat(req.body.percentDiscount) / 100.0;
+    const publisher_param = req.body.publisher;
+
+    try {
+      const books = await Books.updateMany(
+        { publisher: publisher_param },
+        { $mul: { price: percentDiscount } }
+      );
       res.status(200).json(books);
     } catch (error) {
       res.status(404).json({ message: error });
@@ -90,9 +56,9 @@ class BookDetailsController {
 }
 
 // routes
-router.post("/books/create", BookDetailsController.createBook);
-router.get("/books/ISBN/:ISBN", BookDetailsController.getByISBN);
-router.post("/authors/create", BookDetailsController.createAuthor);
-router.get("/authors/:author", BookDetailsController.getByAuthor);
+router.get("/books/genre/:genre", BookBrowsingController.getByGenre);
+router.get("/books/top10", BookBrowsingController.getTopTen);
+router.get("/books/rating/:rating", BookBrowsingController.getByRating);
+router.put("/books/discount", BookBrowsingController.applyDiscount);
 
 module.exports = router;
